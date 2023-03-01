af58bbe333eec883b0963fbb80a60fa4cc9bac42
hassan-imtiaz2 <89877539+hassan-imtiaz2@users.noreply.github.com>
2023-03-02 02:30:53 +0500

    vidly removed

--- vidly project/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-//custom Middleware Function
-function log(req, res, next) {
-    console.log('logging');
-    next() // to pass control to next middleware function in pipeline 
-}    // if we don't pass to next middleware request get hanging
-
-module.exports = log;
